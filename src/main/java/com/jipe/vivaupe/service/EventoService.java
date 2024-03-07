6862cd001ac6ca5550ce7c9e03e9ce51287c0b1d
Ana Oliveira <beatriz.oliveirac@upe.br>
2024-03-07 17:52:05 -0300

    fix: string da hora em details

--- src/main/java/com/jipe/vivaupe/service/EventoService.java
+++ src/main/java/com/jipe/vivaupe/service/EventoService.java
@@ -63,7 +63,7 @@ public class EventoService {
             if (!eventos.isEmpty()){
                 EventoDTO evento = eventos.get(0);
                 return ("O evento: "+ evento.getNome()+ ". Na data: "+ evento.getHora().getHora().substring(0, 10)
-                        + " e de " + evento.getHora().getHora().substring(11, 16) + ". Possui a seguinte descrição: " + evento.getDescricao());
+                        + " e às " + evento.getHora().getHora().substring(11, 16) + " horas. Possui a seguinte descrição: " + evento.getDescricao());
             } else{
                 return ("Não encontrei "+ events + " registrado no google Calendar da U P E .");
             }
